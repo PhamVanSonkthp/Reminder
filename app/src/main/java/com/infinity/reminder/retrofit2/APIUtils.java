4d5509fdd3f91161d24dbf26a1c04c6d7329430a
son <bontukyhpkt@gmail.com>
2021-12-23 16:06:37 +0700

    son

--- app/src/main/java/com/infinity/reminder/retrofit2/APIUtils.java
+++ app/src/main/java/com/infinity/reminder/retrofit2/APIUtils.java
@@ -1,7 +1,7 @@
 package com.infinity.reminder.retrofit2;
 
 public class APIUtils {
-    public static final String baseUrl = "http://20.119.48.31:8088/api/";
+    public static final String baseUrl = "http://20.127.108.161:8088/api/";
 
     public static DataClient getData(){
         return RetrofitClient.getClient(baseUrl).create(DataClient.class);
