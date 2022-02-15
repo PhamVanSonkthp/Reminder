37905b13503c36376b89d2f5b7b7979f8b712bd4
son <bontukyhpkt@gmail.com>
2022-02-15 17:50:05 +0700

    son

--- app/src/main/java/com/infinity/reminder/retrofit2/APIUtils.java
+++ app/src/main/java/com/infinity/reminder/retrofit2/APIUtils.java
@@ -1,7 +1,7 @@
 package com.infinity.reminder.retrofit2;
 
 public class APIUtils {
-    public static final String baseUrl = "http://20.127.78.136:8088/api/";
+    public static final String baseUrl = "http://52.149.215.72:8088/api/";
     public static final String PBAP_UUID = "00001101-0000-1000-8000-00805F9B34FB";
 
     public static DataClient getData(){
