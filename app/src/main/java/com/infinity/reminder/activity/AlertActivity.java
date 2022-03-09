42a7982720c291d0588969cc8dd9b78f9e65a9c4
phamson <bontukyhpkt@gmail.com>
2022-03-09 20:43:33 +0700

    son-2022-03-09

---
+++ app/src/main/java/com/infinity/reminder/activity/AlertActivity.java
@@ -0,0 +1,193 @@
+package com.infinity.reminder.activity;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.infinity.reminder.R;
+import com.infinity.reminder.adapter.AdapterRCVSchedule;
+import com.infinity.reminder.canvas.CustomDateTimePicker;
+import com.infinity.reminder.model_objects.DataSchedule;
+import com.infinity.reminder.retrofit2.APIUtils;
+import com.infinity.reminder.retrofit2.DataClient;
+import com.infinity.reminder.storage.Storager;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class AlertActivity extends AppCompatActivity {
+
+    EditText edtTitle;
+    TextView txtTime;
+
+    RecyclerView rcvRemind;
+    ArrayList<DataSchedule.DataListSchedule> dataSchedules;
+    AdapterRCVSchedule adapterRCVSchedule;
+
+    private int id;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_schedule);
+
+        Toast.makeText(this, ""+id, Toast.LENGTH_SHORT).show();
+
+//        addController();
+//        addEvents();
+//        webService();
+    }
+
+    private void webService() {
+        // fetch data from server
+        DataClient dataClient = APIUtils.getData();
+        Call<DataSchedule> callback = dataClient.getSchedule("Bearer " + Storager.USER_APP.getAccess_token() , id);
+        callback.enqueue(new Callback<DataSchedule>() {
+            @Override
+            public void onResponse(@NonNull Call<DataSchedule> call, @NonNull Response<DataSchedule> response) {
+                if(response.code() == 200){
+                    dataSchedules.clear();
+                    dataSchedules.addAll(response.body().getData().getData());
+                    adapterRCVSchedule.notifyDataSetChanged();
+                    rcvRemind.scrollToPosition( Math.max(0, dataSchedules.size() - 1));
+                }else if (response.code() == 403){
+                    FirebaseMessaging.getInstance().unsubscribeFromTopic("id-" + Storager.USER_APP.getUserData().getRole());
+                    File dir = getFilesDir();
+                    File file = new File(dir, Storager.FILE_INTERNAL);
+                    file.delete();
+
+                    Intent intent = new Intent(AlertActivity.this , LoginActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(AlertActivity.this, response.code() + "", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<DataSchedule> call, @NonNull Throwable t) {
+
+            }
+        });
+    }
+
+    private void addEvents() {
+        txtTime.setOnClickListener(v -> {
+            CustomDateTimePicker custom;
+            custom = new CustomDateTimePicker(this,
+                    new CustomDateTimePicker.ICustomDateTimeListener() {
+
+                        @Override
+                        public void onSet(Dialog dialog, Calendar calendarSelected,
+                                          Date dateSelected, int year, String monthFullName,
+                                          String monthShortName, int monthNumber, int day,
+                                          String weekDayFullName, String weekDayShortName,
+                                          int hour24, int hour12, int min, int sec,
+                                          String AM_PM) {
+                            //                        ((TextInputEditText) findViewById(R.id.edtEventDateTime))
+                            txtTime.setText("");
+                            txtTime.setText(year
+                                    + "-" + (monthNumber + 1) + "-" + calendarSelected.get(Calendar.DAY_OF_MONTH)
+                                    + " " + hour24 + ":" + min
+                                    + ":" + sec);
+                        }
+
+                        @Override
+                        public void onCancel() {
+
+                        }
+                    });
+            /**
+             * Pass Directly current time format it will return AM and PM if you set
+             * false
+             */
+            custom.set24HourFormat(true);
+            /**
+             * Pass Directly current data and time to show when it pop up
+             */
+            custom.setDate(Calendar.getInstance());
+
+            custom.showDialog();
+        });
+    }
+
+    private void addController() {
+        id = getIntent().getIntExtra("id", 0);
+        edtTitle = findViewById(R.id.edt_content);
+        txtTime = findViewById(R.id.txtTime);
+
+        rcvRemind = findViewById(R.id.rcv_remind);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        rcvRemind.setLayoutManager(linearLayoutManager);
+        rcvRemind.setNestedScrollingEnabled(false);
+        dataSchedules = new ArrayList<>();
+        adapterRCVSchedule = new AdapterRCVSchedule(this, dataSchedules);
+        rcvRemind.setAdapter(adapterRCVSchedule);
+
+        if(Storager.USER_APP.getUserData().getRole() == 1){
+            findViewById(R.id.view_bottom).setVisibility(View.GONE);
+        }
+    }
+
+    public void addSchedule(View view) {
+
+        String title = edtTitle.getText().toString();
+        String time = txtTime.getText().toString();
+
+        if(time.equals("Đặt thời gian")){
+            Toast.makeText(this, "Vui lòng chọn thời gian", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(title.length() == 0){
+            Toast.makeText(this, "Vui lòng nhập nội dung", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        DataClient dataClient = APIUtils.getData();
+        Call<String> callback = dataClient.addRemind("Bearer " + Storager.USER_APP.getAccess_token() , title, time , 1 , id);
+        callback.enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(@NonNull Call<String> call, @NonNull Response<String> response) {
+                if(response.code() == 200){
+                    edtTitle.setText("");
+                    webService();
+                }else if (response.code() == 403){
+                    FirebaseMessaging.getInstance().unsubscribeFromTopic("id-" + Storager.USER_APP.getUserData().getRole());
+                    File dir = getFilesDir();
+                    File file = new File(dir, Storager.FILE_INTERNAL);
+                    file.delete();
+
+                    Intent intent = new Intent(AlertActivity.this , LoginActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<String> call, @NonNull Throwable t) {
+
+            }
+        });
+    }
+
+    public void back(View view) {
+        finish();
+    }
+}
--- app/src/main/java/com/infinity/reminder/adapter/AdapterRCVUser.java
+++ app/src/main/java/com/infinity/reminder/adapter/AdapterRCVUser.java
@@ -2,6 +2,7 @@ package com.infinity.reminder.adapter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.infinity.reminder.R;
+import com.infinity.reminder.activity.AlertActivity;
 import com.infinity.reminder.activity.ScheduleActivity;
 import com.infinity.reminder.activity.AirSensorActivity;
 import com.infinity.reminder.activity.Max30100SensorActivity;
@@ -56,9 +58,18 @@ public class AdapterRCVUser extends RecyclerView.Adapter<RecyclerView.ViewHolder
             context.startActivity(intent);
         });
 
+        viewhodler.btnAlert.setOnClickListener(v -> {
+            Intent intent = new Intent(context , AlertActivity.class);
+            intent.putExtra("id" , arrItem.get(holder.getAdapterPosition()).getId());
+            context.startActivity(intent);
+        });
+
         viewhodler.btnHeart.setOnClickListener(v -> {
             Intent intent = new Intent(context , Max30100SensorActivity.class);
             intent.putExtra("id" , arrItem.get(holder.getAdapterPosition()).getId());
+
+            Log.e("AAAA" , arrItem.get(holder.getAdapterPosition()).getId()+"");
+            Log.e("AAAA" , Storager.USER_APP.getAccess_token());
             context.startActivity(intent);
         });
 
@@ -98,7 +109,7 @@ public class AdapterRCVUser extends RecyclerView.Adapter<RecyclerView.ViewHolder
 
     static class Viewhodler extends RecyclerView.ViewHolder {
         TextView  txtName, txtPhone, txtAge;
-        ImageView btnSchedule,btnHeart,btnAir, btnDelete;
+        ImageView btnSchedule,btnHeart,btnAir, btnDelete, btnAlert;
 
         Viewhodler(@NonNull View itemView) {
             super(itemView);
@@ -106,6 +117,7 @@ public class AdapterRCVUser extends RecyclerView.Adapter<RecyclerView.ViewHolder
             txtPhone = itemView.findViewById(R.id.item_rcv_user_txt_phone);
             txtAge = itemView.findViewById(R.id.item_rcv_user_txt_age);
             btnSchedule = itemView.findViewById(R.id.item_rcv_user_btn_schedule);
+            btnAlert = itemView.findViewById(R.id.item_rcv_user_btn_alert);
             btnHeart = itemView.findViewById(R.id.item_rcv_user_btn_heart);
             btnAir = itemView.findViewById(R.id.item_rcv_user_btn_air);
             btnDelete = itemView.findViewById(R.id.item_rcv_user_btn_delete);
